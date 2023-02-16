43672b1032d3938f4d518b1fb6673d298fd7e88d
Lechiffre2110 <73186756+Lechiffre2110@users.noreply.github.com>
2023-02-16 14:52:58 +0100

    [KBE-41] set up spring boot

---
+++ src/test/java/com/bookstore/shippingservice/ShippingserviceApplicationTests.java
@@ -0,0 +1,10 @@
+package com.bookstore.shippingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingserviceApplicationTests {
+
+
+}
